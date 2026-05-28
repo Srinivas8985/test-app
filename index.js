6cf968af65a441c35ead3ce6b53bb3cab3828621
Lakshmi Srinivas <143154164+Srinivas8985@users.noreply.github.com>
2026-05-28 19:08:47 +0530

    Update message in /ci-cd endpoint

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 app.get('/ci-cd', (req, res)=>{
-  res.json({message: "After fixing container information from jenkins again with proper build checking if mongo is getting existed or not and also pulled changes with gokul webhook with jenkins after webhook created skjdks this is from deployment fix"});
+  res.json({message: "After fixing container information from jenkins again with proper build checking if mongo is getting existed or not and also pulled changes with gokul webhook with jenkins after webhook created skjdks this is from deployment fix after turninig off redeploy"});
 })
 app.get('/gokul', (req, res)=>{
   res.json({message: "success"});
