e8cd6da52a63250679b39d6a219762ddae23551a
Lakshmi Srinivas <143154164+Srinivas8985@users.noreply.github.com>
2026-06-30 12:07:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 app.get('/ci-cd', (req, res)=>{
-  res.json({message: "After gokul fixing container information from jenkins again with proper build checking if mongo isd testedkjfd getting existed sldjlsjdor not and also pulled changes with gokul webhook with jenkins after webhook created skjdks this is from deployment fix after turninig off redeploy this is frpm deployment check if local host is removed or not"});
+  res.json({message: "After t"});
 })
 app.get('/gokul', (req, res)=>{
   res.json({message: "success"});
