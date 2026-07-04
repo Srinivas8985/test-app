876fcb01875150e9160cea20bb9fb25d074f526a
Lakshmi Srinivas <143154164+Srinivas8985@users.noreply.github.com>
2026-07-04 20:20:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 app.get('/ci-cd', (req, res)=>{
-  res.json({message: "After testing lwjldsjdljl this is before tcs interview after fixed  jenkins token setted"});
+  res.json({message: "After testing lwjldsjdljl this is before tcs interview after fixed  jenkins token setted after closing connection"});
 })
 app.get('/gokul', (req, res)=>{
   res.json({message: "success"});
